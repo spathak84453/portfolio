0c9086f396984a716dd9e5ca2e8ab6016bb789af
Sachin Pathak <sachinpa@maqsoftware.com>
2026-05-27 21:56:32 +0530

    update production URL in README and configure base path in Vite config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+    <BrowserRouter basename={import.meta.env.BASE_URL} future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
